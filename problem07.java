bfca17b9a577085e86ece1b6949e35d10bd732c7
S <s>
2023-10-05 14:23:42 +0400

    {"types":"C","runs":6}

--- problem07.java
+++ problem07.java
@@ -1,5 +1,17 @@
-import stanford.karel.Karel;
 
-public class problem07 extends Karel{
+import stanford.karel.SuperKarel;
+
+public class problem07 extends SuperKarel{
+	public void run(){
+		move();
+		move();
+		move();
+		if(frontIsClear()){
+			move();
+		} else {
+			turnAround();
+			move();
+		}
+	}
 
 }

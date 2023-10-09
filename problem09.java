dcae4fe7411bd3268b364fdc6c007c02097652db
S <s>
2023-10-09 18:52:24 +0400

    {"types":"C","runs":61}

--- problem09.java
+++ problem09.java
@@ -1,20 +1,60 @@
-import stanford.karel.SuperKarel;
+import stanford.karel.Karel;
 
-public class problem09 extends SuperKarel{
-	public void run(){
+public class problem09 extends Karel {
+	public void run() {
 		move();
-		while(beepersPresent()) {
+		while (beepersPresent()) {
 			pickBeeper();
 			move();
+			transferBeepers();
+		}
+	}
+	
+	// transfers beepers from third to fourth
+	private void transferBeepers() {
+		addBeepers();
+		moveTwice();
+		turnAround();
+		returnBeepers();
+		for (int i = 0; i < 3; i++) {
+			move();
+		}
+		turnAround();
+	}
+	
+	// adds beepers from third to fourth and fifth
+	private void addBeepers() {
+		while (beepersPresent()) {
 			pickBeeper();
+			move();
+			putBeeper();
 			move();
 			putBeeper();
 			turnAround();
-			move();
-			putBeeper();
-			move();
+			moveTwice();
 			turnAround();
 		}
 	}
-
+	
+	// returns beepers from fifth to third
+	private void returnBeepers() {
+		while (beepersPresent()) {
+			pickBeeper();
+			moveTwice();
+			putBeeper();
+			turnAround();
+			moveTwice();
+			turnAround();
+		}
+	}
+	
+	private void turnAround() {
+		turnLeft();
+		turnLeft();
+	}
+	
+	private void moveTwice() {
+		move();
+		move();
+	}
 }

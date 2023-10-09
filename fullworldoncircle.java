f5244b34abef146de32854c8050efd4dbd8cd6b0
S <s>
2023-10-09 16:22:57 +0400

    {"types":"C","runs":35}

--- fullworldoncircle.java
+++ fullworldoncircle.java
@@ -1,4 +1,36 @@
+import stanford.karel.SuperKarel;
 
-public class fullworldoncircle {
+public class fullworldoncircle extends SuperKarel {
+	public void run() {
+		while (true) {
+			if (frontIsClear()) {
+				putBeeper();
+				move();
+				if (beepersPresent()) {
+					turnAround();
+					move();
+					turnRight();
+					move();
+					if (frontIsClear()) {
+						move();
+						if (beepersPresent()) {
+							turnAround();
+							move();
+							putBeeper();
+							turnRight();
+							move();
+							putBeeper();
+							break;
+						}
+						turnAround();
+						move();
+						turnAround();
+					}
+				}
+			} else {
+				turnLeft();
+			}
+		}
+	}
 
 }

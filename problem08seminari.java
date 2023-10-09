263bba76155d572f4bd700be12e41fbed6cbcadb
S <s>
2023-10-09 16:18:01 +0400

    {"types":"C","runs":34}

--- problem08seminari.java
+++ problem08seminari.java
@@ -1,4 +1,37 @@
+import stanford.karel.SuperKarel;
 
-public class problem08seminari {
+public class problem08seminari extends SuperKarel{
+	public void run(){
+		doubleMove();
+		while(beepersPresent()){
+			pickBeeper();
+			moveBack();
+			pickBeeper();
+			move();
+		}
+		moveBack();
+		copy();
+		moveBack();
+	}
 
+	private void copy() {
+		while(beepersPresent()){
+			pickBeeper();
+			doubleMove();
+			putBeeper();
+			moveBack();
+			moveBack();
+		}
+	}
+
+	private void moveBack() {
+		turnAround();
+		move();
+		turnAround();
+	}
+
+	private void doubleMove() {
+		move();
+		move();
+	}
 }

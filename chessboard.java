5016993d7c5a50e7ff6be1c732657ed0bcabc677
S <s>
2023-10-11 21:42:26 +0400

    {"types":"C","runs":49}

--- chessboard.java
+++ chessboard.java
@@ -2,18 +2,10 @@ import stanford.karel.Karel;
 
 public class chessboard extends Karel{
 	 public void run() {
-	        putBeeper();
-	        move();
-	        while (frontIsClear()) {
-	            putBeeperIfClear();
-	        }
-	    }
-
-	    private void putBeeperIfClear() {
-	        move();
-	        if (frontIsClear()) {
-	            putBeeper();
-	            move();
-	        }
-	    }
-	}
+		 putBeeper();
+		 move();
+		 while(frontIsClear()){
+			 move();
+		     if 
+	 }
+}	      
--- chesssboa.java
+++ chesssboa.java
@@ -1,4 +1,15 @@
+import stanford.karel.Karel;
 
-public class chesssboa {
-
+public class chesssboa extends Karel {
+	public void run() {
+		putBeeper();
+		move();
+		while(frontIsClear()) {
+			move();
+			if(frontIsClear()) {
+				putBeeper();
+				move();
+			}
+		}
+	}
 }

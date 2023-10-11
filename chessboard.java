1b7a4bf705b59c2cdf3c3c06f1350282e8e09268
S <s>
2023-10-11 21:28:05 +0400

    {"types":"C","runs":42}

--- chessboard.java
+++ chessboard.java
@@ -3,10 +3,9 @@ import stanford.karel.Karel;
 public class chessboard extends Karel{
 	public void run(){
 		while(frontIsClear()) {
+			move();
+			move();
 			putBeeper();
-			if(frontIsClear()){
-				move();
-			}
 		}
 	}
 }

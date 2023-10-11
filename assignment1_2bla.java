3c42f75acb8f8478e87c3ac3dfeb0eb262aacf8c
S <s>
2023-10-11 20:01:50 +0400

    {"types":"C","runs":27}

--- assignment1_2bla.java
+++ assignment1_2bla.java
@@ -1,4 +1,13 @@
+import stanford.karel.Karel;
 
-public class assignment1_2bla {
-
+public class assignment1_2bla extends Karel{
+	public void run() {
+		turnLeft();
+		while(frontIsClear()) {
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+			move();
+		}
+	}
 }

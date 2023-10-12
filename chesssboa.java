de97944be96a1b4992e11ca024631d464839fb6a
S <s>
2023-10-12 16:39:09 +0400

    {"types":"C","runs":56}

--- chesssboa.java
+++ chesssboa.java
@@ -3,9 +3,10 @@ import stanford.karel.Karel;
 public class chesssboa extends Karel {
 	public void run() {
 		putBeeper();
-		//move();
-		while(frontIsClear()) {
+		if(frontIsClear()) {
 			move();
+		}
+		while(frontIsClear()) {
 			move();
 			if(frontIsClear()) {
 				putBeeper();

1c02ef4df70389fe2d920844355d81c11b24947b
S <s>
2023-10-13 00:37:37 +0400

    {"types":"C","runs":217}

--- assignment1_4.java
+++ assignment1_4.java
@@ -1,4 +1,35 @@
-
-public class assignment1_4 {
-
+import stanford.karel.*;
+public class assignment1_4 extends SuperKarel {
+public void run(){
+//Karel put the first and last beeper
+   putBeeper();
+placeLastBeeper();
+//Karel move beepers towards the midle one by one
+while(noBeepersPresent()){
+moveBeeper();
+}
+//Karel pick the last beeper and return in to the middle
+pickBeeper();
+turnAround();
+move();
+}
+//Karel find the middle
+private void moveBeeper(){ 
+    while(noBeepersPresent()){
+         move();
+    }
+     pickBeeper();
+     turnAround();
+     move();
+     putBeeper();
+     move();
+    }
+private void placeLastBeeper(){
+    while(frontIsClear()){
+         move();
+    }
+     putBeeper();
+     turnAround();
+     move();
+}
 }

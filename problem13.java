74973e81d6266705fe5417e0000658f47d76fd49
S <s>
2023-10-12 18:54:02 +0400

    {"types":"C","runs":115}

--- problem13.java
+++ problem13.java
@@ -30,9 +30,9 @@ public class problem13 extends GraphicsProgram{
 			double x;
 			int y = (int) (getHeight() - HOUSE_HEIGHT + (int) (WINDOW_HEIGHT / 2));
 			if(i == 0) {
-				x = getWidth() /2 - HOUSE_WIDTH / 2;
+				x = getWidth() /2 - HOUSE_WIDTH / 2 + WINDOW_WIDTH / 2;
 			} else {
-				x = getWidth() /2 - HOUSE_WIDTH /2 + WINDOW_WIDTH;
+				x = getWidth() /2 - HOUSE_WIDTH /2 - WINDOW_WIDTH *3 / 2;
 			}
 			GRect window = new GRect(WINDOW_WIDTH, WINDOW_HEIGHT);
 			add(window, x, y);

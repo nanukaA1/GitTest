1bba83fb54f44adb756cca5b137bc489034900e3
S <s>
2023-10-12 22:06:37 +0400

    {"types":"C","runs":154}

--- jg.java
+++ jg.java
@@ -2,17 +2,9 @@ import stanford.karel.Karel;
 
 public class jg extends Karel {
 	public void run() {
-		putBeeper();
-		if(frontIsClear()) {
-			move();
-		}
 		while(frontIsClear()) {
 			move();
-			if(frontIsClear()) {
-				putBeeper();
-				//move();
-			}
-			move();
+			turnLeft();
 		}
 		
 	}

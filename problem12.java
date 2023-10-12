de291950d8f94b79e5fa53452e0c9fe8c9966d9e
S <s>
2023-10-12 18:30:30 +0400

    {"types":"C","runs":111}

--- problem12.java
+++ problem12.java
@@ -1,18 +1,18 @@
 
+import acm.graphics.GLine;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class problem12 extends GraphicsProgram{
 	public void run() {
-		int width = 100;
-		width = width + 20;
-		width = 200;
-		
-		GRect rect = new GRect(width, width, width, width);
-		add(rect);
+		drawLine();
 	}
-	
-	private void bestVoid() {
-		
+	private void drawLine() {
+		double x1 = 0;
+		double y1 = getHeight() / 2;
+		double x2 = getWidth();
+		double y2 = y1 ;
+		GLine line = new GLine(x1, y1, x2, y2);
+		add(line);
 	}
 }

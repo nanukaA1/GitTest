3c1f2719d6b87a1a1d7aa02d206a6e198c025e8e
S <s>
2023-10-13 00:41:04 +0400

    {"types":"C","runs":218}

--- assignment1_4.java
+++ assignment1_4.java
@@ -1,35 +1,40 @@
 import stanford.karel.*;
 public class assignment1_4 extends SuperKarel {
-public void run(){
-//Karel put the first and last beeper
-   putBeeper();
-placeLastBeeper();
-//Karel move beepers towards the midle one by one
-while(noBeepersPresent()){
-moveBeeper();
+	public void run(){
+		while(frontIsClear()) {
+			putBeeper();
+			move();
+		}
+	}
 }
-//Karel pick the last beeper and return in to the middle
-pickBeeper();
-turnAround();
-move();
-}
-//Karel find the middle
-private void moveBeeper(){ 
-    while(noBeepersPresent()){
-         move();
-    }
-     pickBeeper();
-     turnAround();
-     move();
-     putBeeper();
-     move();
-    }
-private void placeLastBeeper(){
-    while(frontIsClear()){
-         move();
-    }
-     putBeeper();
-     turnAround();
-     move();
-}
-}
+////Karel put the first and last beeper
+//   putBeeper();
+//placeLastBeeper();
+////Karel move beepers towards the midle one by one
+//while(noBeepersPresent()){
+//moveBeeper();
+//}
+////Karel pick the last beeper and return in to the middle
+//pickBeeper();
+//turnAround();
+//move();
+//}
+////Karel find the middle
+//private void moveBeeper(){ 
+//    while(noBeepersPresent()){
+//         move();
+//    }
+//     pickBeeper();
+//     turnAround();
+//     move();
+//     putBeeper();
+//     move();
+//    }
+//private void placeLastBeeper(){
+//    while(frontIsClear()){
+//         move();
+//    }
+//     putBeeper();
+//     turnAround();
+//     move();
+//}

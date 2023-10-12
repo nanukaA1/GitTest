f35b9ef454a5774f360c3a11e67fa2a930054658
S <s>
2023-10-12 17:16:53 +0400

    {"types":"C","runs":77}

--- chesssboa.java
+++ chesssboa.java
@@ -2,36 +2,35 @@ import stanford.karel.Karel;
 
 public class chesssboa extends Karel {
 	public void run() {
-		//putBeeper();
-		while(frontIsClear()) {
-			putBeeper();
 		if(frontIsClear()) {
-			move();
-		}
-		while(frontIsClear()) {
-			move();
-			if(frontIsClear()) {
-				putBeeper();
-				move();
-			}
-		}
-		if(facingWest()) {
-			turnLeft();
-			turnLeft();
-			turnLeft();
+			putBeeper();
 			if(frontIsClear()) {
 				move();
-				turnLeft();
-				turnLeft();
-				turnLeft();
 			}
-		} else {
-			turnLeft();
-			if(frontIsClear()) {
+			while(frontIsClear()) {
 				move();
+				if(frontIsClear()) {
+					putBeeper();
+					move();
+				}
+			}
+			if(facingWest()) {
 				turnLeft();
+				turnLeft();
+				turnLeft();
+				if(frontIsClear()) {
+					move();
+					turnLeft();
+					turnLeft();
+					turnLeft();
+				}
+			} else {
+				turnLeft();
+				if(frontIsClear()) {
+					move();
+					turnLeft();
+				}
 			}
 		}
-	}
 	}
 }

79952c112321b2a4706a7e830bb710be5d645088
S <s>
2023-10-12 22:11:42 +0400

    {"types":"C","runs":159}

--- TurnLeftIfOddKarel.java
+++ TurnLeftIfOddKarel.java
@@ -2,16 +2,16 @@ import stanford.karel.*;
 
 public class TurnLeftIfOddKarel extends SuperKarel {
 
+
     public void run() {
-        putBeeper(); // Place the initial beeper
         while (frontIsClear()) {
             move();
-            putBeeper();
-        }
-
-        if (beepersPresent()) {
-            // The number of cells is odd, so turn left
-            turnLeft();
+            if (frontIsBlocked()) {
+                turnLeft();
+            }
+            if (frontIsClear()) {
+                move();
+            }
         }
     }
 }

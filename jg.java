5279ac96761002bd1e672db9b02731f065f90ec2
S <s>
2023-10-12 21:12:30 +0400

    {"types":"C","runs":143}

--- jg.java
+++ jg.java
@@ -1,5 +1,17 @@
-
+import stanford.karel.Karel;
 
 public class jg extends Karel {
+	public void run() {
+		defineOddOrEven();
+	}
+	
+	private void defineOddOrEven() {
+		while(frontIsClear()) {
+			move();
+			if(frontIsClear()) {
+				move();
+			}
+		}
+	}
 	
 }

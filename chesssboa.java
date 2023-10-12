7cd1ba6ede4562e846fd3ee6be41f91ed4252666
S <s>
2023-10-12 17:29:08 +0400

    {"types":"C","runs":85}

--- chesssboa.java
+++ chesssboa.java
@@ -33,8 +33,8 @@ public class chesssboa extends Karel {
 			}
 		}
 		if(frontIsBlocked()) {
-			turnLeft();
 			putBeeper();
+			turnLeft();
 			while(frontIsClear()) {
 				move();
 				if(frontIsClear()) {

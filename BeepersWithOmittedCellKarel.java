77b431ac39ad0da1c9ef7ab5622d6d9172b8df8c
S <s>
2023-10-12 20:08:56 +0400

    {"types":"C","runs":132}

--- BeepersWithOmittedCellKarel.java
+++ BeepersWithOmittedCellKarel.java
@@ -3,16 +3,36 @@ import stanford.karel.*;
 public class BeepersWithOmittedCellKarel extends SuperKarel {
 
     public void run() {
+        putBeepersEvenRow();
+        while (frontIsClear()) {
+            move();
+            putBeepersOddRow();
+            if (frontIsClear()) {
+                move();
+                putBeepersEvenRow();
+            }
+        }
+    }
+
+    private void putBeepersEvenRow() {
         putBeeper();
         while (frontIsClear()) {
             move();
-            putBeeper();
             if (frontIsClear()) {
                 move();
                 putBeeper();
             }
+        }
+    }
+
+    private void putBeepersOddRow() {
+        move();
+        putBeeper();
+        while (frontIsClear()) {
+            move();
             if (frontIsClear()) {
                 move();
+                putBeeper();
             }
         }
     }

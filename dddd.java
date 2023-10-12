6d03096d2562a9a218b64fc805eee5b8c9ac1974
S <s>
2023-10-12 22:51:30 +0400

    {"types":"C","runs":178}

--- dddd.java
+++ dddd.java
@@ -1,4 +1,23 @@
+import stanford.karel.Karel;
 
-public class dddd {
-
+public class dddd extends Karel {
+	public void run() {
+		fillEvenRow();
+		//returnBackAndMoveUp();
+	}
+	private void fillEvenRow() {
+		while(frontIsClear()) {
+			putBeeper();
+			if(frontIsClear()) {
+				move();
+			}
+			while(frontIsClear()) {
+				move();
+				if(frontIsClear()) {
+					putBeeper();
+					move();
+				}
+			}
+		}
+	}
 }

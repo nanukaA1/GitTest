811a8041ca2e188492f4221e85eec3f6bdfdd355
S <s>
2023-10-17 18:56:50 +0400

    {"types":"C","runs":12}

--- problem17.java
+++ problem17.java
@@ -2,14 +2,12 @@ import acm.program.ConsoleProgram;
 
 public class problem17 extends ConsoleProgram{
 	public void run() {
-		//კონსოლიდან წაიკითხეთ ორი მთელი რიცხვი და დაბეჭდეთ მათ შორის მაქსიმალური.
-		int a = readInt("enter first number: ");
-		int b = readInt("enter second number: ");
+		double a = readInt("enter number: ");
 		
-		if(a>b) {
-			println(a);
-		} else {
-			println(b);
-		}
+		int b = (int) a;
+		println(b);
+		
+		double c = a - b;
+		println(c);
 	}
 }

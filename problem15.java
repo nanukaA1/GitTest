94e2c5056bbc66b9037c27901cc153d4a78f6b4c
S <s>
2023-10-17 18:22:44 +0400

    {"types":"AR","runs":0}

---
+++ problem15.java
@@ -0,0 +1,9 @@
+import acm.program.ConsoleProgram;
+
+public class problem15 extends ConsoleProgram{
+	public void run() {
+		int firstNum = readInt("Enter First Number: ");
+		int secondNum = readInt("Enter Second Number: ");
+		
+	}
+}

c0972a30182341454dee3576e2145e6fabba010b
S <s>
2023-10-31 20:42:53 +0400

    {"types":"C","runs":6}

--- problem11.java
+++ problem11.java
@@ -1,11 +1,37 @@
-import stanford.karel.SuperKarel;
+import stanford.karel.*;
 
-public class problem11 extends SuperKarel{
-	public void run(){
-		while(true) {
-			move();
-			if(facingNorth() &&)
-		}
-	}
+public class problem11 extends SuperKarel {
 
+    public void run() {
+        while (!atUpperRightCorner()) {
+            moveAlongWall();
+        }
+    }
+
+    // Check if Karel is at the upper right corner
+    private boolean atUpperRightCorner() {
+        return frontIsBlocked() && rightIsBlocked();
+    }
+
+    // Move along the wall until reaching the upper right corner
+    private void moveAlongWall() {
+        if (frontIsClear()) {
+            move();
+        } else {
+            turnLeft();
+        }
+    }
+
+    // Check if there is a wall in front of Karel
+    private boolean frontIsBlocked() {
+        return !frontIsClear();
+    }
+
+    // Check if there is a wall to the right of Karel
+    private boolean rightIsBlocked() {
+        turnRight();
+        boolean isBlocked = frontIsBlocked();
+        turnLeft(); // Turn back to the original direction
+        return isBlocked;
+    }
 }

f8ecb91dba84c6d0cd468e9673bd765a559f7b50
S <s>
2023-11-01 20:27:27 +0400

    {"types":"C","runs":65}

--- problem29.java
+++ problem29.java
@@ -6,28 +6,23 @@ public class problem29 extends ConsoleProgram {
 	
 			int n = readInt("enter n: ");
 			
-			
 			println(fib(n));
 		
 	}
 
-	
-//	indices: 0 1 2 3 4 5 6
-//	values:  0 1 1 2 3 5 8
 	private int fib(int n) {
 		if (n == 0 || n == 1) {
 			return n;
 		}
 		
-		int prev2 = 0;
-		int prev1 = 1;
+		int a = 0;
+		int b = 1;
 		
-//		loop
 		int curr = -1;
 		for (int i = 2; i <= n; i++) {
-			curr = prev2 + prev1;
-			prev2 = prev1;
-			prev1 = curr;
+			curr = b + a;
+			a = b;
+			b = curr;
 		}
 		
 		return curr;

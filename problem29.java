492ae9178b728054a55c5c9ae3e48443d5700eb2
S <s>
2023-11-01 20:25:29 +0400

    {"types":"C","runs":62}

--- problem29.java
+++ problem29.java
@@ -1,27 +1,35 @@
-import acm.program.GraphicsProgram;
+import acm.program.ConsoleProgram;
 
-public class problem29 extends GraphicsProgram{
+public class problem29 extends ConsoleProgram {
+
 	public void run() {
-		int n = readInt("enter n: ");
-		int fib = nthFibonacciNum(n)
-		println(fib);
-	}
 	
-	private int nthFibonacciNum(int n) {
+			int n = readInt("enter n: ");
+			
+			
+			println(fib(n));
+		
+	}
+
+	
+//	indices: 0 1 2 3 4 5 6
+//	values:  0 1 1 2 3 5 8
+	private int fib(int n) {
 		if (n == 0 || n == 1) {
 			return n;
 		}
 		
-		int a = 0;
-		int b = 1;
+		int prev2 = 0;
+		int prev1 = 1;
 		
-		int x = 0;
-		for(int i = 2; i <= n; i++) {
-			x = a + b;
-			a = b;
-			b = x;
+//		loop
+		int curr = -1;
+		for (int i = 2; i <= n; i++) {
+			curr = prev2 + prev1;
+			prev2 = prev1;
+			prev1 = curr;
 		}
-		return x;
+		
+		return curr;
 	}
-
 }

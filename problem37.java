a1822a9a6684fd7ed854150afffe76911998ce86
S <s>
2023-11-02 19:40:02 +0400

    {"types":"C","runs":54}

--- problem37.java
+++ problem37.java
@@ -8,12 +8,13 @@ public class problem37 extends ConsoleProgram {
 		int rouletteNum = randomGeneratedNumFrom0To36();
 		
 		println(rouletteNum);
-		
+		while(userNum != rouletteNum){
 		if(userNum == rouletteNum) {
 			println("you win"); 
+			break;
 		} else {
 			println("you lose");
-		}
+		}}
 	}
 	
 	private int randomGeneratedNumFrom0To36() {

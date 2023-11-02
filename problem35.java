e9160cf5a7074c4909eb5bad0dce7ea2dec57740
S <s>
2023-11-02 18:57:57 +0400

    {"types":"C","runs":22}

--- problem35.java
+++ problem35.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
 public class problem35 extends ConsoleProgram {
-	private static final int NUM_EXPERIMENTS = 10;
+	private static final int NUM_EXPERIMENTS = 10000;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run() {
 		double sumOfFlips = 0;
@@ -18,7 +18,7 @@ public class problem35 extends ConsoleProgram {
 		int numFlips = 0;
 		while(true) {
 			numFlips += 1;
-			boolean isHeads = rgen.nextBoolean();
+			boolean isHeads = rgen.nextBoolean(); //თუ ყალბი მონეტაა და ვთქვათ ალბათობა 9 მაშინ 0.1-ს ჩავწერდით
 			if(isHeads) {
 				break;
 			}

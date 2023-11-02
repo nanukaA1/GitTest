524df54d41463453c8c9cea2d17a7bbb4676da21
S <s>
2023-11-02 18:38:16 +0400

    {"types":"AR","runs":11}

--- problem35.java
+++ problem35.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class problem34 extends GraphicsProgram {
+public class problem35 extends GraphicsProgram {
 	public void run() {
 		
 	}

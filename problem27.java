32ad5c3fc955ec81c8efdde1f6993e48604a03f6
S <s>
2023-11-06 20:18:48 +0400

    {"types":"C","runs":3}

--- problem27.java
+++ problem27.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import acm.program.ConsoleProgram;
 
 public class problem27 extends ConsoleProgram{
@@ -26,33 +26,4 @@ public class problem27 extends ConsoleProgram{
 		}
 		return x;
 	}
-=======
-import acm.program.ConsoleProgram;
-
-public class problem27 extends ConsoleProgram{
-	public void run() {
-		int n = readInt("Enter n: ");
-		int m = readPositiveInt("Enter m: "); 
-		
-		int x = pow(n, m);
-		println(n + "^" + m + " = " + x);
-	}
-
-	private int readPositiveInt(String text) {
-		int z = readInt(text);
-		while( z < 0) {
-			println("Number should be non negative");
-			z = readInt(text);
-		}
-		return z;
-	}
-	private int pow(int n, int m) {
-		int x = n; 
-		for (int i = 0; i < m-1; i++) {
-			x *= n; 
-			
-		}
-		return x;
-	}
->>>>>>> 550ae8305d99d169daea069a75673d4f57327e24
 }

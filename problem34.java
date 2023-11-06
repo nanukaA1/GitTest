7a31d5446df7d625c88bacfedd28b1ab13d64c28
S <s>
2023-11-06 20:20:50 +0400

    {"types":"C","runs":4}

--- problem34.java
+++ problem34.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.awt.Color;
 
 import acm.graphics.GOval;
@@ -27,33 +26,4 @@ public class problem34 extends GraphicsProgram {
 		add(circle, x, y);
 	}
 }
-=======
-import java.awt.Color;
 
-import acm.graphics.GOval;
-import acm.program.GraphicsProgram;
-import acm.util.RandomGenerator;
-
-public class problem34 extends GraphicsProgram {
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private static final int MIN_RADIUS = 5;
-	private static final int MAX_RADIUS = 10;
-	public void run() {
-		for(int i = 0; i < 1000; i++) {
-			addRandomCircle();
-		}
-	}
-	
-	private void addRandomCircle() {
-		int radius = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
-		double x = rgen.nextDouble(0,getWidth() - 2 * radius);
-		double y = rgen.nextDouble(0,getHeight() - 2 * radius);
-		Color color = rgen.nextColor();
-		GOval circle = new GOval(2 * radius, 2 * radius);
-		circle.setFilled(true);
-		circle.setColor(color);
-		
-		add(circle, x, y);
-	}
-}
->>>>>>> 550ae8305d99d169daea069a75673d4f57327e24
--- problem36.java
+++ problem36.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 //თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ
@@ -35,41 +35,4 @@ public class problem36 extends ConsoleProgram {
 		return numFlips;
 	}
 }
-=======
-import acm.program.ConsoleProgram;
-import acm.util.RandomGenerator;
-//თქვენი ამოცანაა გააკეთოთ მონეტის აგდების სიმულაციები და დათვალოთ საშუალოდ
-//რამდენჯერ უნდა ავაგდოთ მონეტა რათა 3-ჯერ ზედიზედ ამოვიდეს ბორჯღალო.
-public class problem36 extends ConsoleProgram {
-	private static final int NUM_EXPERIMENTS = 10000;
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
-	public void run() {
-		double sumOfFlips = 0;
-		for(int i = 0; i < NUM_EXPERIMENTS; i++) {
-			int currNumFlips = holdExperiment();
-			sumOfFlips += currNumFlips;
-		}
-		double avgFlips = sumOfFlips / NUM_EXPERIMENTS;
-		println("avg number of flips: " + avgFlips);
-	}
-	
-	private int holdExperiment() {
-		int numFlips = 0;
-		int numConsecutiveHeads = 0;
-		while (true) {
-			numFlips += 1;
-			boolean isHeads = rgen.nextBoolean(0.4);
-			if (isHeads) {
-				numConsecutiveHeads += 1;
-				if (numConsecutiveHeads == 3) {
-					break;
-				}
-			} else {
-				numConsecutiveHeads = 0;
-			}
-		}
-		return numFlips;
-	}
-}
->>>>>>> 550ae8305d99d169daea069a75673d4f57327e24
+

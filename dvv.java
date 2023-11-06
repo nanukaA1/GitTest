75ba4f9085bf9f419283fbb3f97e45088c9cdc07
S <s>
2023-11-06 23:18:24 +0400

    {"types":"C","runs":49}

--- dvv.java
+++ dvv.java
@@ -1,4 +1,8 @@
+import acm.program.ConsoleProgram;
 
-public class dvv {
-
+public class dvv extends ConsoleProgram {
+public void run() {
+	double RADIUS2 = 72 * 1.65 / 2.54;
+	println(RADIUS2);
+}
 }

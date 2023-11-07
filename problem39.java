d3e085376e6347b38e5a6ea5c17e1b1ee5b3b421
S <s>
2023-11-07 18:43:28 +0400

    {"types":"C","runs":10}

--- problem39.java
+++ problem39.java
@@ -1,4 +1,13 @@
+import java.awt.Color;
 
-public class problem39 {
+import acm.graphics.GOval;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
+public class problem39 extends GraphicsProgram {
+	private static final int PAUSETIME = 1000;
+	public void run() {
+		GOval ball = new GOval(20, 20);
+		add(ball, 0, getHeight()/ 2);
+	}
 }

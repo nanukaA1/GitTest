38d9626e0c9c96651a406f316b5159d1323c0a06
S <s>
2023-11-07 18:50:48 +0400

    {"types":"C","runs":22}

--- problem39.java
+++ problem39.java
@@ -10,8 +10,8 @@ public class problem39 extends GraphicsProgram {
 		GOval ball = new GOval(20, 20);
 		add(ball, 0, getHeight()/ 2);
 		
-		while(ball.getX() < getWidth() - 5);
-		ball.move(5, 0);
+		while(ball.getX() < getWidth() - 20);
+		ball.move(5, 7);
 		pause(PAUSETIME);
 	}
 }

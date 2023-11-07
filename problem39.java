b3174aea274b58c6b723d2747b703f7a71dabe2d
S <s>
2023-11-07 19:00:17 +0400

    {"types":"C","runs":32}

--- problem39.java
+++ problem39.java
@@ -7,13 +7,14 @@ import acm.program.GraphicsProgram;
 public class problem39 extends GraphicsProgram {
 	private static final int RADIUS = 20;
 	private static final int PAUSETIME = 10;
+
 	public void run() {
 		GOval ball = new GOval(RADIUS, RADIUS);
-		add(ball, 0, getHeight()/ 2);
-		
-		while(ball.getX() <= getWidth() - 20){
-		ball.move(5, 0);
-		pause(PAUSETIME);
+		add(ball, 0, getHeight() / 2);
+
+		while (ball.getX() <= getWidth() - 20) {
+			ball.move(5, 0);
+			pause(PAUSETIME);
 		}
 	}
 }

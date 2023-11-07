a6c77e7bf7b3052426ee637ede27b136dcbc8682
S <s>
2023-11-07 18:36:57 +0400

    {"types":"C","runs":5}

--- problem39.java
+++ problem39.java
@@ -1,4 +1,18 @@
+import java.awt.Color;
 
-public class problem39 {
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
+public class problem39 extends GraphicsProgram {
+	private static final int PAUSETIME = 1000;
+	public void run() {
+		GRect myRect = new GRect(100, 200);
+		myRect.setFilled(true);
+		add(myRect, 100, 150);
+		
+		myRect.setColor(Color.RED);
+		pause(PAUSETIME);
+		myRect.setColor(Color.GREEN);
+		
+	}
 }

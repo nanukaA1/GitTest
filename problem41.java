e9710c0ba26faf443aab9173618006fc1976bd72
S <s>
2023-11-09 18:51:14 +0400

    {"types":"C","runs":58}

--- problem41.java
+++ problem41.java
@@ -1,4 +1,21 @@
+import java.awt.Color;
 
-public class problem41 {
+import com.sun.glass.events.KeyEvent;
+import com.sun.glass.events.MouseEvent;
 
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+public class problem41 extends GraphicsProgram {
+	public void run() {
+		addMouseListeners();
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		GOval dot = new GOval(1, 1);
+		while(true) {
+			add(dot, e.getX(), e.getY());
+		}
+	}
 }

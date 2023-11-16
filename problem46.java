790ed2bf6908e663442af82d9b75e81f91520fab
S <s>
2023-11-16 18:54:02 +0400

    {"types":"C","runs":72}

--- problem46.java
+++ problem46.java
@@ -1,4 +1,28 @@
+import acm.program.ConsoleProgram;
 
-public class problem46 {
+public class problem46 extends ConsoleProgram {
+	public void run() {
+		String str = readLine();
+		mostFrequentChar(str);
+	}
 
+	private int mostFrequentChar(String string) {
+		int count1 = 0;
+		int count2 = 0;
+		char ch;
+
+		for (int i = 0; i < string.length(); i++) {
+			ch = string.charAt(i);
+			for (int j = 0; j < string.length(); j++) {
+				if (ch == string.charAt(j)) {
+					count1++;
+				}
+			}
+			if(count1 > count2) {
+				count2 = count1;
+			}
+		}
+		return count2 - 1;
+
+	}
 }

f785d1d326e3e334bd27ae28f67e3a3555fcfa17
S <s>
2023-11-28 16:50:36 +0400

    {"types":"C","runs":52}

--- problem35.java
+++ problem35.java
@@ -5,22 +5,25 @@ import acm.util.RandomGenerator;
 public class problem35 extends ConsoleProgram {
 	private static final int NUM_EXPERIMENTS = 10000;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
+
 	public void run() {
 		double sumOfFlips = 0;
-		for(int i = 0; i < NUM_EXPERIMENTS; i++) {
+		for (int i = 0; i < NUM_EXPERIMENTS; i++) {
 			int currNumFlips = holdExperiment();
 			sumOfFlips += currNumFlips;
 		}
 		double avgFlips = sumOfFlips / NUM_EXPERIMENTS;
 		println("avg number of flips: " + avgFlips);
 	}
-	
+
 	private int holdExperiment() {
 		int numFlips = 0;
-		while(true) {
+		while (true) {
 			numFlips += 1;
-			boolean isHeads = rgen.nextBoolean(); //თუ ყალბი მონეტაა და ვთქვათ ალბათობა 9 მაშინ 0.1-ს ჩავწერდით
-			if(isHeads) {
+			boolean isHeads = rgen.nextBoolean(); // თუ ყალბი მონეტაა და ვთქვათ
+													// ალბათობა 9 მაშინ 0.1-ს
+													// ჩავწერდით
+			if (isHeads) {
 				break;
 			}
 		}

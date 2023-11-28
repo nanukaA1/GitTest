7e42ec4ec6b1b1096902c927438ffca5a78a2132
S <s>
2023-11-28 19:05:19 +0400

    {"types":"C","runs":69}

--- problem50.java
+++ problem50.java
@@ -1,4 +1,30 @@
+import java.util.StringTokenizer;
 
-public class problem50 {
+import acm.program.ConsoleProgram;
+
+//დაწერეთ მეთოდი რომელიც გადაცემული ტექსტიდან შლის დუბლირებულ სიმბოლოებს,
+//ანუ თუ ტექსტიში რომელიმე სიმბოლო მეორდება, უნდა დარჩეს მხოლოდ ყველაზე მარცხენა.
+public class problem50 extends ConsoleProgram {
+	public void run() {
+
+		String text = readLine();
+		println(isInAscendingOrder(text));
+	}
+
+	private Boolean isInAscendingOrder(String text) {
+		 char prevChar = 'a'; // Starting with 'a'
+	        for (char currentChar : text.toCharArray()) {
+	            if (Character.isLowerCase(currentChar)) {
+	                if (currentChar < prevChar) {
+	                    return false;
+	                }
+	                prevChar = currentChar;
+	            }
+	        }
+	        return true;
+	}
+
+	
+	}
 
 }

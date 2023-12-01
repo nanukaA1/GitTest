28603182ef74999c82020ce9476b46149f83a511
S <s>
2023-12-01 23:47:02 +0400

    {"types":"C","runs":227}

--- problem51.java
+++ problem51.java
@@ -13,14 +13,14 @@ public class problem51 extends ConsoleProgram {
 	private String compresse(String input) {
 		String newText = "";
 		char currentChar = input.charAt(0);
-		int count = 0;
+		int count = 1;
 		for (int i = 1; i < input.length(); i++) {
 			if (input.charAt(i) == currentChar) {
 				count++;
 			} else {
 				newText += Integer.toString(count) + currentChar;
 				currentChar = input.charAt(i);
-				count = 0;
+				count = 1;
 			}
 		}
 		return newText;

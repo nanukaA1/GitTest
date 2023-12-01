e32c8df693c8ab532604fb589ca14cb4d73fcd7c
S <s>
2023-12-01 21:43:49 +0400

    {"types":"C","runs":207}

--- problem51.java
+++ problem51.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class problem51 {
+public class problem51 extends ConsoleProgram{
+	public void run() {
+		while(true) {
+			String text = readLine("Enter text: ");
+			String editedText = removeDuplicates(text);
+			println(editedText);
+		}
+	}
+	
+	private String removeDuplicates(String text) {
+		String result = "";
+		for(int i = 0; i < text.length(); i++) {
+			char ch  = text.charAt(i);
 
+			if(result.indexOf(ch) == -1) {
+				result += ch;
+			}
+		}
+		return result;
+	}
 }

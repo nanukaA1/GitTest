8e8dea9a065dff7f1632ad916b7cb7db35537be6
S <s>
2023-12-01 23:50:47 +0400

    {"types":"C","runs":232}

--- problem51.java
+++ problem51.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class problem51 extends ConsoleProgram {
 	public void run() {
 
-		String input = "aabbbbcc";
+		String input = "aabbbbccddd";
 		int n = 3;
 		String result = compresse(input);
 		println(result);

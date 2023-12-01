cbdb21b1c1af44f500e1d11c27d6bad812ea39a4
S <s>
2023-12-01 13:10:50 +0400

    {"types":"C","runs":85}

--- problem38.java
+++ problem38.java
@@ -1,4 +1,51 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class problem38 {
+public class problem38 extends ConsoleProgram {
+	private static final int MIN_VALUE = 0;
+	private static final int MAX_VALUE = 36;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		int remAmount = 1000;
+		
+		while(remAmount > 0) {
+			int betNumber = readBetNumber();
+			int betAmount = readBetAmount(remAmount);
+			int rouletteResult = spinRoulette();
+			if(betNumber == rouletteResult) {
+				remAmount += betAmount;
+				println("You won this turn!");
+			}else {
+				remAmount -= betAmount;
+				println("You lost this turn!");
+			}
+			println("Remaining amount: " + remAmount);
+		}
+		println("GAME OVER, you lost.");
+		
+	}
 
+	private int readBetAmount(int remAmount) {
+		while(true) {
+			int betAmount = readInt("Enter bet Amount(should be <= " + remAmount + "): ");
+			if(betAmount <= remAmount) {
+				return betAmount;
+			}
+		}
+	}
+
+	private int readBetNumber() {
+		while(true) {
+			int betNum = readInt("Enter bet number from " + MIN_VALUE + " to " + MAX_VALUE + ": ");
+			if(betNum >= MIN_VALUE && betNum <= MAX_VALUE) {
+				return betNum;
+			}
+		}
+	}
+	
+	private int spinRoulette() {
+		int rouletteResult = rgen.nextInt(MIN_VALUE, MAX_VALUE);
+		println("Roulette result is: " + rouletteResult);
+		return rouletteResult;
+	}
 }

46a21cfb6452fe862c251130dd0483d5e35888b3
S <s>
2023-12-01 23:39:08 +0400

    {"types":"C","runs":220}

--- problem51.java
+++ problem51.java
@@ -3,38 +3,25 @@ import acm.program.ConsoleProgram;
 public class problem51 extends ConsoleProgram{
 	public void run() {
 	
-			 String input = "aaabbbccc";
+			 String input = "aabbbbcc";
 		        int n = 3;
-		        String result = removeNplet(input, n);
+		        String result = compresse(input);
 		        println(result);
 		
 	}
 	
-	private String removeNplet(String input, int n) {
-        // Check if the length of the input string is less than n
-        if (input.length() < n) {
-            return input;
-        }
-
-        // Iterate through the string to find and remove nplets
-        for (int i = 0; i <= input.length() - n; i++) {
-            // Check if the current substring is an nplet
-            boolean isNplet = true;
-            for (int j = 1; j < n; j++) {
-                if (input.charAt(i) != input.charAt(i + j)) {
-                    isNplet = false;
-                    break;
-                }
-            }
-
-            // If it's an nplet, remove it and recursively call the method
-            if (isNplet) {
-                String newInput = input.substring(0, i) + input.substring(i + n);
-                return removeNplet(newInput, n);
-            }
-        }
-
-        // If no more nplets are found, return the modified string
-        return input;
+	private String compresse(String input) {
+		String newText = "";
+		char currentChar = input.charAt(0);
+		int count = 0;
+       for(int i =1 ; i < input.length(); i++) {
+    	   if (input.charAt(i) == currentChar) {
+               count++;
+           } else {
+        	   newText = "" + count + currentChar;
+        	   currentChar = input.charAt(i);
+           }
+       }
+       return newText;
     }
 }

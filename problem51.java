9ba07008b4b18864a82bf3e62cb53d380e62b30c
S <s>
2023-12-01 23:43:11 +0400

    {"types":"C","runs":223}

--- problem51.java
+++ problem51.java
@@ -1,28 +1,28 @@
 import acm.program.ConsoleProgram;
 
-public class problem51 extends ConsoleProgram{
+public class problem51 extends ConsoleProgram {
 	public void run() {
-	
-			 String input = "aabbbbcc";
-		        int n = 3;
-		        String result = compresse(input);
-		        println(result);
-		
+
+		String input = "aabbbbcc";
+		int n = 3;
+		String result = compresse(input);
+		println(result);
+
 	}
-	
+
 	private String compresse(String input) {
 		String newText = "";
 		char currentChar = input.charAt(0);
 		int count = 0;
-       for(int i =1 ; i < input.length()+1; i++) {
-    	   if (input.charAt(i) == currentChar) {
-               count++;
-           } else {
-        	   newText = "" + count + currentChar;
-        	   currentChar = input.charAt(i);
-        	   count = 0;
-           }
-       }
-       return newText;
-    }
+		for (int i = 1; i < input.length(); i++) {
+			if (input.charAt(i) == currentChar) {
+				count++;
+			} else {
+				newText = "" + count + currentChar;
+				currentChar = input.charAt(i);
+				count = 0;
+			}
+		}
+		return newText;
+	}
 }

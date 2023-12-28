6a49f11a51e052517251866e610c35a85442659c
S <s>
2023-12-28 22:20:14 +0400

    {"types":"C","runs":22}

--- problem01.java
+++ problem01.java
@@ -1,12 +1,21 @@
-import stanford.karel.Karel;
+import acm.program.ConsoleProgram;
 
-public class problem01 extends Karel {
+public class problem01 extends ConsoleProgram {
 	public void run() {
-		move();
-		move();
-		pickBeeper();
-		move();
-		putBeeper();
+		int[] input = new int[]{1, 1, 3, 7, 7, 8, 9, 9, 9, 10};
+		int current = input[0];
+		boolean found = false;
+
+		for (int i = 0; i < input.length; i++) {
+		    if (current == input[i] && !found) {
+		        found = true;
+		    } else if (current != input[i]) {
+		        System.out.print(" " + current);
+		        current = input[i];
+		        found = false;
+		    }
+		}
+		System.out.print(" " + current)
 	}
 
 }

18d561dc090a74c7c48ad54d4f98c715cb992087
S <s>
2024-01-05 17:52:23 +0400

    {"types":"C","runs":4}

--- fghvm.java
+++ fghvm.java
@@ -2,12 +2,25 @@ import acm.graphics.GLine;
 import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 
-public class fghvm extends ConsoleProgram {
+public class fghvm extends GraphicsProgram {
 	public void run() {
-		int a = readInt();
-		int b = readInt();
-		double x = (double)(a+b)/2;
-		println(x);
+		drawGrid();
+	}
+
+	private void drawGrid() {
+		addVertical();
+		// addHorizontal();
+	}
+
+	private void addVertical() {
+		double x = 0;
+		double y1 = 0;
+		double y2 = getHeight();
+		GLine line = new GLine(x, y1, x, y2);
+		for (int i = 0; i < 11; i++) {
+			x += i * getWidth() / 10;
+			add(line);
+		}
 	}
 
 }

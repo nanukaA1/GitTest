76961515c258a664c69992007376cbca41edc54b
S <s>
2024-01-06 01:55:17 +0400

    {"types":"C","runs":96}

--- problem49.java
+++ problem49.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class problem49 {
+public class problem49 extends ConsoleProgram {
+	public void run() {
+		String numStr = "12340";
+		int num = stringToInteger(numStr);
 
+		println(num);
+		println(num - 1);
+		println(num + 1);
+	}
+
+	private int stringToInteger(String numStr) {
+		int num = 0;
+		for (int i = 0; i < numStr.length(); i++) {
+			num = num * 10 + (numStr.charAt(i) - '0');
+		}
+		return num;
+	}
 }

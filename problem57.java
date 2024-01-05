771eb13bbb03fa4fa445f169f9958db2df5e0c12
S <s>
2024-01-06 03:35:15 +0400

    {"types":"C","runs":121}

--- problem57.java
+++ problem57.java
@@ -4,8 +4,8 @@ import acm.program.ConsoleProgram;
 
 public class problem57 extends ConsoleProgram {
 	public void run() {
-		String str1 = "mariam";
-		String str2 = "mraiam";
+		String str1 = "acghh";
+		String str2 = "hacgh";
 		println(areAnagrams(str1, str2));
 	}
 
@@ -13,17 +13,16 @@ public class problem57 extends ConsoleProgram {
 		if(str1.equals(str2)) {
 			return false;
 		}
-		int[] counts1 = getCountArray(str1);
-		int[] counts2 = getCountArray(str2);
+		int[] counts1 = getCount(str1);
+		int[] counts2 = getCount(str2);
 		
-		println("counts for debug");
 		println(Arrays.toString(counts1));
 		println(Arrays.toString(counts2));
 		
 		return Arrays.equals(counts1, counts2);
 	}
 	
-	private int[] getCountArray(String s) {
+	private int[] getCount(String s) {
 		int[] counts = new int[26];
 		for(int i = 0; i < s.length(); i++) {
 			char ch = s.charAt(i);

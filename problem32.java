51fc5fce82163ff5c24ed83c8fa6e69be20b2a74
S <s>
2024-01-05 19:06:28 +0400

    {"types":"C","runs":29}

--- problem32.java
+++ problem32.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
 
-public class problem32 {
+public class problem32 extends ConsoleProgram {
+	public void run() {
+		for (int i = 2; i <= 100000; i += 2) {
+			if (isPrime(i)) {
+				println(i);
+			}
+		}
+	}
 
+	private boolean isPrime(int n) {
+		int countDivisors = countDivs(n);
+		return countDivisors == 2;
+	}
+
+	private int countDivs(int n) {
+		int count = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				count++;
+			}
+		}
+		return count;
+	}
 }

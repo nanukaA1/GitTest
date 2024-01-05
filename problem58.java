cb7b8e0b8331b2560a73c9eb2b1c055e2fb8fb33
S <s>
2024-01-06 03:44:08 +0400

    {"types":"C","runs":122}

--- problem58.java
+++ problem58.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class problem58 {
+public class problem58 extends ConsoleProgram{
+	public void run() {
+		int[][] matrix = {
+				{1, 2, 3},
+				{9, 7, 8},
+				{4, 5, 6}};
+		println(matrix.length);
+		println(isMagical(matrix));
+	}
 
+	private String isMagical(int[][] matrix) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

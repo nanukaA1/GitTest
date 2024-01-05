5fbd2dcf667ecaac3c6e04fcd11b0971555e77fd
S <s>
2024-01-05 19:10:14 +0400

    {"types":"C","runs":33}

--- problem31.java
+++ problem31.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class problem31 {
+public class problem31 extends ConsoleProgram {
+	public void run() {
+		println(isPrime());
+	}
+
+	private boolean isPrime() {
+		int b = readInt();
+		int x = 0;
+		for (int i = 1; i <= b; i++) {
+			if (b % i == 0) {
+				x++;
+			}
+		}
+		return x == 2;
+	}
 
 }

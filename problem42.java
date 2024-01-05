7c80012bac567959fd6e1bb521bfc8d76215c803
S <s>
2024-01-05 22:33:58 +0400

    {"types":"C","runs":58}

--- problem42.java
+++ problem42.java
@@ -1,27 +1,29 @@
+
 import java.awt.event.MouseEvent;
 
-import acm.graphics.GLine;
-import acm.program.GraphicsProgram;
+import acm.graphics.*;
+import acm.program.*;
 
-public class problem42 extends GraphicsProgram{
-	private double x1 = 0;
-	private double y1 = 0;
-	private double x2 = 0;
-	private double y2 = 0;
-	public void run() {
-		addMouseListeners();
-	}
-	
-	public void mousePressed(MouseEvent e) {
-		x1 = getX();
-		y1 = getY();
-	}
-	
-	public void mouseDragged(MouseEvent e) {
-		x2 = getX();
-		y2 = getY();
-		
-		GLine line = new GLine(x1,y1,x2,y2);
-		add(line);
-	}
+public class problem42 extends GraphicsProgram {
+
+    private GLine activeLine;
+
+    public void init() {
+        addMouseListeners();
+    }
+
+    public void run() {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        activeLine = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
+        add(activeLine);
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        activeLine.setEndPoint(e.getX(), e.getY());
+    }
 }

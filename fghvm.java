159dc1580541487a006dd2d95dfd2cf15b72328e
S <s>
2024-01-05 18:36:28 +0400

    {"types":"C","runs":17}

--- fghvm.java
+++ fghvm.java
@@ -2,7 +2,7 @@ import acm.graphics.GLine;
 import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 
-public class fghvm extends GraphicsProgram {
+public class fghvm extends ConsoleProgram {
 	public void run() {
 		int a = readInt();
 		int b = readInt();

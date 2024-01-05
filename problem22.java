ce1d5a28e9a4f1910bba42f6ce85185e7364f980
S <s>
2024-01-05 18:24:16 +0400

    {"types":"C","runs":13}

--- problem22.java
+++ problem22.java
@@ -7,7 +7,7 @@ public class problem22 extends ConsoleProgram {
 	public void run() {
 		int a = readInt();
 		int x = 0;
-		for(int i = 1; i < a; i ++) {
+		for(int i = 1; i <= a; i ++) {
 			if(a % i == 0) {
 				x++;
 			}

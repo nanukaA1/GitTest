05b297ae2801ff9a338f4971e04bae122858b7dd
S <s>
2024-01-05 22:55:12 +0400

    {"types":"C","runs":65}

--- problem43.java
+++ problem43.java
@@ -1,16 +1,38 @@
 
+import java.awt.event.MouseEvent;
+
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
-public class problem43 extends GraphicsProgram{
+public class problem43 extends GraphicsProgram {
+	private GOval circle;
+	private int x;
+	private int y;
+
 	public void run() {
 		addCircle();
 		addMouseListeners();
-		//drag();
+	}
+	public void mousePressed(MouseEvent e) {
+		if (getElementAt(e.getX(), e.getY()) != null) {
+			x = e.getX();
+			y = e.getY();
+		}
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		if (getElementAt(e.getX(), e.getY()) != null) {
+			circle.move(e.getX() - x, e.getY() - y);
+			// update values
+			x = e.getX();
+			y = e.getY();
+		}
 	}
 
 	private void addCircle() {
-		GOval circle = new GOval(50, 50);
+		circle = new GOval(50, 50);
+		circle.setFilled(true);
 		add(circle, getWidth() / 2 - 25, getHeight() / 2 - 25);
 	}
+
 }

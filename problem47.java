77a0405b69c5ce0dce937b230c001064463b4bf1
S <s>
2024-01-06 01:37:24 +0400

    {"types":"C","runs":89}

--- problem47.java
+++ problem47.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class problem47 {
+public class problem47 extends ConsoleProgram {
+	public void run() {
+		String text = readLine();
+		println(isPalindrom(text));
+	}
 
+	private boolean isPalindrom(String text) {
+		for(int i = 0; i < text.length() / 2; i++) {
+			if(text.charAt(i) != text.charAt(text.length() - 1 - i)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }

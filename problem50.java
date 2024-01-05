204d0a23d70c1593bf012e837a611834d32f913a
S <s>
2024-01-06 02:17:42 +0400

    {"types":"C","runs":101}

--- problem50.java
+++ problem50.java
@@ -5,7 +5,7 @@ import acm.program.ConsoleProgram;
 public class problem50 extends ConsoleProgram {
 	public void run() {
 		String text = readLine();
-		StringTokenizer tokenizer = new StringTokenizer(text, "l");
+		StringTokenizer tokenizer = new StringTokenizer(text, " ");
 
 		int count = 0;
 		while (tokenizer.hasMoreTokens()) {

15143af73a73d631ad025f65ea7bda399ffbdc9d
S <s>
2024-01-06 01:49:48 +0400

    {"types":"C","runs":92}

--- problem48.java
+++ problem48.java
@@ -1,4 +1,33 @@
+import acm.program.ConsoleProgram;
 
-public class problem48 {
+public class problem48 extends ConsoleProgram{
+	public void run() {
+		String text = readLine();
+		printStatistics(text);
+	}
 
+	private void printStatistics(String text) {
+		for(int i = 0; i < text.length(); i++) {
+			if(isFirstOccurance(text, text.charAt(i), i)) {
+				println(text.charAt(i) + ": " + symbolCount(text, text.charAt(i)));
+			}
+		}
+	}
+
+	private int symbolCount(String text, char ch) {
+		int count = 0;
+		for(int i = 0; i < text.length(); i++) {
+			if(ch == text.charAt(i)) {
+				count ++;
+			}
+		}
+		return count;
+	}
+
+	private boolean isFirstOccurance(String text, char ch, int i) {
+		if(text.indexOf(ch) == i) {
+			return true;
+		}
+		return false;
+	}
 }

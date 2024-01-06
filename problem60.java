633172b25aa9958e59de0e1f6276b3edfe26104d
S <s>
2024-01-06 04:05:33 +0400

    {"types":"C","runs":128}

--- problem60.java
+++ problem60.java
@@ -1,18 +1,17 @@
 import java.util.ArrayList;
-
 import acm.program.ConsoleProgram;
 
-public class problem60 extends ConsoleProgram{
-	public void run(){
+public class problem60 extends ConsoleProgram {
+	public void run() {
 		ArrayList<Integer> list = new ArrayList<>();
-		while(true){
+		while (true) {
 			int n = readInt("Enter number: ");
-			if(n == -1){
+			if (n == -1) {
 				break;
 			}
 			list.add(0, n);
 		}
-		for(int next : list){
+		for (int next : list) {
 			println(next);
 		}
 	}
--- problem61.java
+++ problem61.java
@@ -1,4 +1,42 @@
 
-public class problem61 {
 
+import java.util.HashSet;
+import java.util.Set;
+
+import acm.program.*;
+import javafx.util.Pair;
+
+public class problem61 extends ConsoleProgram {
+		
+	public void run() {
+		int n = readInt();
+		int m = readInt();
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < n; i ++) {
+			arr[i] = readInt();
+		}
+		Pair p = getSum(arr, m);
+		if (p == null) {
+			println("Pair not found!");
+		} else {
+			println(p);
+		}
+	}
+
+	private Pair getSum(int[] arr, int sum) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < arr.length; i ++) {
+			if (2 * arr[i] == sum && set.contains(arr[i])) {
+				return new Pair(arr[i], arr[i]);
+			}
+			set.add(arr[i]);
+		}
+		for (Integer key : set) {
+			if (set.contains(sum - key)) {
+				println(new Pair(key, sum - key));
+			}
+		}
+		return null;
+	}
 }

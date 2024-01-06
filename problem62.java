5ffffe57da5c5580990d16a68cf74b354f2e81bf
S <s>
2024-01-06 04:09:02 +0400

    {"types":"CR","runs":129}

--- problem62.java
+++ problem62.java
@@ -1,4 +1,17 @@
+import java.util.HashMap;
 
-public class problem62 {
+import acm.program.ConsoleProgram;
 
+public class problem62 extends ConsoleProgram{
+	public void run() {
+		String s;
+		HashMap<String, Integer> map = new HashMap<>();
+		while (!(s = readLine()).isEmpty()) {
+			map.put(s, map.getOrDefault(s, 0) + 1);
+		}
+		
+		for (String name : map.keySet()) {
+			println(name + " : " + map.get(name));
+		}
+	}
 }

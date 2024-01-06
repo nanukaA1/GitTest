b83c277205ef0706a138487cf21879cfdde0285a
S <s>
2024-01-06 04:29:41 +0400

    {"types":"C","runs":138}

--- problem69.java
+++ problem69.java
@@ -2,81 +2,101 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
+import javax.swing.JTextField;
 
+import acm.graphics.GCompound;
+import acm.graphics.GLabel;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class problem69 extends GraphicsProgram 
-	implements ComponentListener{
-
+public class problem69 extends GraphicsProgram {
+	private static final int DISTANCE = 40;
+	private JTextField textField;
+	private JButton button;
+	private double x = DISTANCE;
+	private double y = DISTANCE;
+	private GCompound messages;
+	private double prevY;
 	public void init() {
-		addComponentListener(this);
-		add(new JButton("     RECOLOR     "), SOUTH);
+		textField = new JTextField(20);
+		add(textField, SOUTH);
+		
+		textField.addActionListener(this);
+
+		
+		button = new JButton("Enter");
+		add(button, SOUTH);
+		
+		
+		messages = new GCompound();
+		add(messages);
 		addActionListeners();
+		addMouseListeners();
+		
 	}
-	
+
+
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		for (int i = 0; i < rectColors.length; i++) {
-			for (int j = 0; j < rectColors[i].length; j++) {
-				rectColors[i][j] = null;
-			}
+		println("here");
+		if(e.getSource() == textField || e.getSource() == button) {
+			String text = textField.getText();
+			println(text);
+			displayMessage(text);
+			textField.setText("");
 		}
-		draw();
 	}
 	
-	public void run() {
-		rectColors = new Color[GRID_NUM][GRID_NUM];
-		draw();
+	private void displayMessage(String text) {
+		if(text == null || text.equals("")) {
+			return;
+		}
+		GLabel message = new GLabel(text);
+		messages.add(message, x, y);
+		y += DISTANCE;
+		
+		fixPositionAtLastMessage();
 	}
-	
-	private static final int GRID_NUM = 8;
-	private RandomGenerator generator = RandomGenerator.getInstance();
-	private Color[][] rectColors;
-	
-	private void draw() {
-		removeAll();
-		double width = (double)getWidth() / GRID_NUM;
-		double height = (double)getHeight() / GRID_NUM;
-		for (int i = 0; i < GRID_NUM; i++) {
-			for (int j = 0; j < GRID_NUM; j++) {
-				GRect rect = new GRect(i * width, j * height, 
-						width, height);
-				rect.setFilled(true);
-				if (rectColors[i][j] == null) {
-					rectColors[i][j] = generator.nextColor();
-				}
-				rect.setColor(rectColors[i][j]);
-				add(rect);
-			}
+
+
+
+	private void fixPositionAtLastMessage() {
+		if(messages.getHeight() + DISTANCE > getHeight()) {
+			double newY = getMinY(); //getHeight() - messages.getHeight() - DISTANCE;
+			messages.setLocation(messages.getX(), newY);
 		}
 		
 	}
 	
-	@Override
-	public void componentHidden(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	private double getMinY() {
+		return getHeight() - messages.getHeight() - DISTANCE;
 	}
-
+	
 	@Override
-	public void componentMoved(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void mousePressed(MouseEvent e) {
+		prevY = e.getY();
 	}
-
+	
 	@Override
-	public void componentResized(ComponentEvent arg0) {
-		draw();
-	}
-
-	@Override
-	public void componentShown(ComponentEvent arg0) {
-		// TODO Auto-generated method stub
+	public void mouseDragged(MouseEvent e) {
+		if(messages.getHeight() + DISTANCE <= getHeight()) {
+			return;
+		}
+		messages.move(0, e.getY() - prevY);
+		prevY = e.getY();
 		
+		double minY = getMinY();
+		double maxY = 0;
+		if (messages.getY() < minY) {
+			messages.setLocation(messages.getX(), minY);
+		}else if(messages.getY() > maxY) {
+			messages.setLocation(messages.getX(), maxY);
+		}
 	}
 	
 }

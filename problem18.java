b862ae76fac9a04a1989ce4131cc42d0520fca09
S <s>
2024-01-08 12:54:07 +0400

    {"types":"C","runs":61}

--- problem18.java
+++ problem18.java
@@ -1,66 +1,16 @@
-import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 
-import javax.swing.JButton;
+import acm.program.ConsoleProgram;
 
-import acm.graphics.GCanvas;
-import acm.graphics.GRect;
-import acm.program.GraphicsProgram;
-import acm.util.RandomGenerator;
-
-public class problem18 extends GraphicsProgram 
-	implements ComponentListener{
-
-	public void init() {
-		addComponentListener(this);
-		add(new JButton("     RECOLOR     "), SOUTH);
-		addActionListeners();
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		for (int i = 0; i < rectColors.length; i++) {
-			for (int j = 0; j < rectColors[i].length; j++) {
-				rectColors[i][j] = null;
-			}
-		}
-		draw();
-	}
-	
+public class problem18 extends ConsoleProgram{
 	public void run() {
-		rectColors = new Color[GRID_NUM][GRID_NUM];
-		draw();
-	}
-	
-	private static final int GRID_NUM = 8;
-	private RandomGenerator generator = RandomGenerator.getInstance();
-	private Color[][] rectColors;
-	
-	private void draw() {
-		removeAll();
-		double width = (double)getWidth() / GRID_NUM;
-		double height = (double)getHeight() / GRID_NUM;
-		for (int i = 0; i < GRID_NUM; i++) {
-			for (int j = 0; j < GRID_NUM; j++) {
-				GRect rect = new GRect(i * width, j * height, 
-						width, height);
-				rect.setFilled(true);
-				if (rectColors[i][j] == null) {
-					rectColors[i][j] = generator.nextColor();
-				}
-				rect.setColor(rectColors[i][j]);
-				add(rect);
-			}
-		}
+		int a = readInt("enter first number: ");
+		int b = readInt("enter second number: ");
 		
+		if(a > b) {
+			println(a);
+		} else {
+			println(b);
+		}
 	}
-
-	public void componentResized(ComponentEvent arg0) {
-		draw();
-	}
-
-
-	
 }
+

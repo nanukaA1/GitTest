99b5b9ac57f17f0bac3ce0caf0e02da8723b4ed9
S <s>
2024-01-08 13:04:17 +0400

    {"types":"C","runs":67}

--- problem01.java
+++ problem01.java
@@ -1,13 +1,19 @@
+import java.util.StringTokenizer;
+
 import stanford.karel.Karel;
 
 public class problem01 extends Karel{
 
 	public void run(){
-		move();
-		move();
-		pickBeeper();
-		move();
-		putBeeper();
+		String line = "Abigayle 0 0 0 0 0 0 0 0 0 0 720";
+		String name = line.substring(0, line.indexOf(" "));
+		StringTokenizer tokenizer = new StringTokenizer(line, " ");
+		int i = 0;
+		int[] ranking = new int[11];
+		while (tokenizer.hasMoreTokens()) {
+			ranking[i] = Integer.parseInt(tokenizer.nextToken());
+			i++;
 	}
+		println(name);
 
-}
+}}

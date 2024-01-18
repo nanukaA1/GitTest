3108252c3732f025cd1e3cb347d6290f7984a17c
S <s>
2024-01-18 16:45:00 +0400

    {"types":"C","runs":4}

--- problem62.java
+++ problem62.java
@@ -3,28 +3,30 @@ import java.util.Map;
 
 import acm.program.ConsoleProgram;
 
-public class problem62 extends ConsoleProgram{
+public class problem62 extends ConsoleProgram {
 	private static final String SENTINEL = "";
+
 	public void run() {
-		Map<String, Integer> nameCounts = 
-				new HashMap<String, Integer>();
-		
-		while(true) {
-			String name = readLine("Enter name: ");
-			if(name.equals(SENTINEL)) {
-				break;
-			}
-			
-			if(!nameCounts.containsKey(name)) {
+		Map<String, Integer> nameCounts = new HashMap<String, Integer>();
+
+		while (true) {
+			String name = readLine();
+
+			if (!nameCounts.containsKey(name)) {
 				nameCounts.put(name, 0);
 			}
-			
+
 			int newCount = nameCounts.get(name) + 1;
 			nameCounts.put(name, newCount);
+			
+			if (name.equals(SENTINEL)) {
+				break;
+			}
+
 		}
-		
+
 		println(nameCounts);
-		
+
 	}
 
 }

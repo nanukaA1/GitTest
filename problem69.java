c6ffcfd2f651ecdaafa5a3c25fb42bc689940b75
S <s>
2024-01-21 15:19:29 +0400

    {"types":"C","runs":78}

--- problem69.java
+++ problem69.java
@@ -1,63 +1,43 @@
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
+import java.awt.event.ActionEvent;
 
-import acm.graphics.GLine;
-import acm.graphics.GOval;
+import javax.swing.JTextField;
+
+import acm.graphics.*;
 import acm.program.GraphicsProgram;
-
-public class problem69 extends GraphicsProgram implements ComponentListener {
+//67. გააკეთეთ ფოტოების აპლიკაცია. აპლიკაციას უნდა ჰქონდეს ტექსფილდი სადაც შეიძლება
+//ფოტოს(ფაილის) მისამართის ჩაწერა. ენტერზე დაჭერის შემდეგ ეს ფოტო უნდა გამოჩნდეს
+//ფანჯრის ცენტრში.
+public class problem69 extends GraphicsProgram{
+	private JTextField textField;
+	
+	// სემინარზე რანში გვქონდა თავიდან დამატებული და უცნაურად იმიტომ გაიხსნა.
 	public void init() {
-		drawGrid();
-		addComponentListener(this);
+		textField = new JTextField(20);
+		add(textField, SOUTH);
+		
+		textField.addActionListener(this);
+		addActionListeners();
+		
+		GImage image = new GImage("bad path");
+		double x = getWidth()/ 2 - image.getWidth() / 2;
+		double y = getHeight()/ 2 - image.getHeight() / 2;
+		add(image, x, y);
 	}
-
-	private void drawGrid() {
-		removeAll();
-		for (int i = 0; i < 10; i++) {
-			addVerticalLine(i);
-			addHorisontalLine(i);
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == textField) {
+			try {
+				GImage image = new GImage(textField.getText());
+				double x = getWidth()/ 2 - image.getWidth() / 2;
+				double y = getHeight()/ 2 - image.getHeight() / 2;
+				add(image, x, y);
+			} catch(Exception ex) {
+				removeAll();
+				add(new GLabel("Error occured"), 20, 20);
+			}
+			textField.setText("");
 		}
-	}
-
-	private void addHorisontalLine(int i) {
-		double height = (double) getHeight() / 9;
-		double y = i * height;
-		double x1 = 0;
-		double x2 = getWidth();
-
-		GLine line = new GLine(x1, y, x2, y);
-		add(line);
-	}
-
-	private void addVerticalLine(int i) {
-		double width = (double) getWidth() / 9;
-		double x = i * width;
-		double y1 = 0;
-		double y2 = getHeight();
-
-		GLine line = new GLine(x, y1, x, y2);
-		add(line);
-	}
-
-	public void componentResized(ComponentEvent e) {
-		drawGrid();
-	}
-
-	@Override
-	public void componentHidden(ComponentEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void componentMoved(ComponentEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void componentShown(ComponentEvent e) {
-		// TODO Auto-generated method stub
-
 	}
 }

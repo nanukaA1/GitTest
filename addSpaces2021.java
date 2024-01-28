840305c9f30b2e1b0f998e50478cbe9e71aa0af9
S <s>
2024-01-29 01:12:57 +0400

    {"types":"C","runs":34}

--- addSpaces2021.java
+++ addSpaces2021.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class addSpaces2021 {
+public class addSpaces2021 extends ConsoleProgram {
+	public void run() {
+		String text = readLine();
+		int[] spaces = { 5, 6, 7 };
+		println(addSpaces(text, spaces));
+	}
 
+	private String addSpaces(String text, int[] spaces) {
+		for(int i = 0; i < spaces.length; i++) {
+			int index = spaces[i] + i;
+			text = text.substring(0, index) + " " + text.substring(index);
+		}
+		return text;
+	}
 }

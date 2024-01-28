667e2f21b94ff1a4df416d2cd8f1c9841b6702e4
S <s>
2024-01-28 22:59:58 +0400

    {"types":"C","runs":10}

--- SchoolGraphics.java
+++ SchoolGraphics.java
@@ -6,36 +6,56 @@ import javax.swing.JTextField;
 import acm.program.GraphicsProgram;
 
 public class SchoolGraphics extends GraphicsProgram {
-	private JTextField teacherField;
-	private JTextField subjectField;
-	private JTextField pupilField;
-	private JButton displayPupils;
-	private JButton displayTeachers;
+	private static final int RESULT_X = 20;
+	private static final int LINE_H = 20;
+
+	private JTextField teachField;
+	private JTextField subjField;
+	private JTextField pupField;
+
+	private JButton teachButton;
+	private JButton subjButton;
+	private JButton pupButton;
+	private JButton displayPup;
+	private JButton displayTeach;
+
 	private School school;
 
 	public void run() {
+		this.resize(1000, 500);
 		school = new School();
-		teacherField = new JTextField(10);
-		add(teacherField, SOUTH);
 
-		subjectField = new JTextField(10);
-		add(subjectField, SOUTH);
+		teachField = new JTextField(10);
+		add(teachField, SOUTH);
 
-		pupilField = new JTextField(10);
-		add(pupilField, SOUTH);
+		subjField = new JTextField(10);
+		add(subjField, SOUTH);
 
-		displayPupils = new JButton("display pupils");
-		add(displayPupils, SOUTH);
+		pupField = new JTextField(10);
+		add(pupField, SOUTH);
 
-		displayTeachers = new JButton("display teachers");
-		add(displayTeachers, SOUTH);
-		teacherField.addActionListener(this);
-		addActionListeners();
+		teachButton = new JButton("Add Teach");
+		add(teachButton, SOUTH);
+
+		subjButton = new JButton("Add Subj");
+		add(subjButton, SOUTH);
+
+		pupButton = new JButton("Add Pup");
+		add(pupButton, SOUTH);
+
+		displayPup = new JButton("Display Pup");
+		add(displayPup, SOUTH);
+
+		displayTeach = new JButton("Display Teach");
+		add(displayTeach, SOUTH);
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == teacherField) {
 			school.addTeacher(teacherField.getText());
+		}
+		if (e.getSource() == pupilField) {
+			school.addPupils(pupilField.getText());
 		}
 	}
 }

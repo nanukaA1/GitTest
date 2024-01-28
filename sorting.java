99b6d0792ff2284c360d74da085608a56519d10e
S <s>
2024-01-29 00:26:28 +0400

    {"types":"C","runs":24}

--- sorting.java
+++ sorting.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 
 import acm.program.ConsoleProgram;
@@ -17,5 +18,16 @@ public class sorting extends ConsoleProgram {
 		int[] arr = {90,17,7,80,9};
 		Arrays.sort(arr);
 		println(Arrays.toString(arr));
+		
+		String[] arr1 = { "lin", "linda", "lind"}
+		Arrays.sort(arr1);
+		println(Arrays.toString(arr1));
+		
+		ArrayList<Integer> list1 = new ArrayList<Integer>();
+		list1.add(readInt());
+		list1.add(readInt());
+		list1.add(readInt());
+		Collections.sort(list1);
+		println(list1);
 	}
 }

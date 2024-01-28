cdc40b934f2dde7fbeab0713405d00ec105ba196
S <s>
2024-01-29 00:19:19 +0400

    {"types":"C","runs":17}

--- comparator.java
+++ comparator.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.Comparator;
 
-public class comparator {
+import acm.program.ConsoleProgram;
 
+public class comparator extends ConsoleProgram {
+	public void run() {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add(readLine());
+		list.add(readLine());
+		list.add(readLine());
+		Comparator<String> c = Comparator.comparing(String::length);
+		list.sort(c);
+		println(list);
+	}
 }

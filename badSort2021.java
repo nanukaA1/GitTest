00a3a057b89d97fc64d2243b656ca4d2dec85760
S <s>
2024-01-29 00:53:59 +0400

    {"types":"C","runs":31}

--- badSort2021.java
+++ badSort2021.java
@@ -24,8 +24,8 @@ public class badSort2021 extends ConsoleProgram{
 		}
 	}
 	private boolean sorted() {
-		for(int i = 0; i < arr.length - 1; i++) {
-			if(arr[i] > arr[i+1]) {
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] < arr[i-1]) {
 				return false;
 			}	
 		}

aaee53bbc36c72aa3ff1784f4dbbc5bc260644d5
S <s>
2024-01-29 21:01:20 +0400

    {"types":"C","runs":65}

--- mamashviloba2014.java
+++ mamashviloba2014.java
@@ -1,4 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class mamashviloba2014 {
+import acm.program.ConsoleProgram;
+
+public class mamashviloba2014 extends ConsoleProgram {
+	public void run() {
+		getCousins("raghac.java", "glenn");
+	}
+
+	private ArrayList<String> getCousins(String fileName, String name) {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			while (true) {
+				String newLine = reader.readLine();
+				if (newLine == null) {
+					return null;
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			return null;
+
+		} catch (IOException e) {
+			return null;
+		}
+	}
 
 }

3bc451afd01a4f97b2cc7679a3bdc47a6a1bde97
S <s>
2024-01-29 19:29:05 +0400

    {"types":"C","runs":61}

--- sorted2022.java
+++ sorted2022.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class sorted2022 {
+import acm.program.ConsoleProgram;
 
+public class sorted2022 extends ConsoleProgram {
+	public void run() {
+		almostSorted(new ArrayList<>(Arrays.asList(2, 3, 4, 5)));
+	}
+
+	private boolean almostSorted(ArrayList<Integer> a) {
+		for (int i = 0; i < a.size(); i++) {
+			for (int j = i + 1; j < a.size(); j++) {
+				swapElements(a,i,j);
+				reverseElements(a, i, j);
+				if (Sorted(a)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	private boolean Sorted(ArrayList<Integer> a) {
+		for (int i = 1; i < a.size(); i++) {
+			if (a.get(i) < a.get(i - 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private void swapElements(ArrayList<Integer> a, int i, int j) {
+		int tmp = a.get(i);
+		a.set(i, a.get(j));
+		a.set(j, tmp);
+	}
+
+	private void reverseElements(ArrayList<Integer> a, int i, int j) {
+		int mid = (j - i + 1) / 2;
+		for (int k = 0; k < mid; k++) {
+			swapElements(a, i + k, j - k);
+		}
+	}
 }

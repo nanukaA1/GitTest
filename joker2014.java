943a1e1f6970fd1eef78d6dc152ae7e9a06e027a
S <s>
2024-01-29 23:18:20 +0400

    {"types":"C","runs":80}

--- joker2014.java
+++ joker2014.java
@@ -10,8 +10,8 @@ public class joker2014 extends ConsoleProgram {
 	  double  occ = 0;
 		for (int i = 0; i < 10000; i++) {
 			 boolean[] deck = new boolean[36];
-		        deck[34] = true; // First joker
-		        deck[35] = true; // Second joker
+		        deck[34] = true;
+		        deck[35] = true;
 		        
 		        for (int j = deck.length - 1; j > 0; j--) {
 		            int index = (int) (Math.random() * (i + 1));
@@ -20,12 +20,10 @@ public class joker2014 extends ConsoleProgram {
 		            deck[i] = temp;
 		        }
 
-            boolean[] player1 = new boolean[9]; // Player 1's hand
-            boolean[] player2 = new boolean[9]; // Player 2's hand
-            boolean[] player3 = new boolean[9]; // Player 3's hand
-            boolean[] player4 = new boolean[9]; // Player 4's hand
-
-            // Deal cards to players
+            boolean[] player1 = new boolean[9]; 
+            boolean[] player2 = new boolean[9]; 
+            boolean[] player3 = new boolean[9]; 
+            boolean[] player4 = new boolean[9]; 
             for (int j = 0; j < 9; j++) {
                 player1[j] = deck[j];
                 player2[j] = deck[j + 9];

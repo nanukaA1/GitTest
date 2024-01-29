3d004b34a0eda087310eb356794a2f4f0479b0be
S <s>
2024-01-29 23:19:17 +0400

    {"types":"C","runs":81}

--- joker2014.java
+++ joker2014.java
@@ -2,10 +2,10 @@ import acm.program.ConsoleProgram;
 
 public class joker2014 extends ConsoleProgram {
 	public void run() {
-		println(simulateJoker(5));
+		println(simulateJoker());
 	}
 
-	private double simulateJoker(int n) {
+	private double simulateJoker() {
 		
 	  double  occ = 0;
 		for (int i = 0; i < 10000; i++) {
@@ -21,14 +21,9 @@ public class joker2014 extends ConsoleProgram {
 		        }
 
             boolean[] player1 = new boolean[9]; 
-            boolean[] player2 = new boolean[9]; 
-            boolean[] player3 = new boolean[9]; 
-            boolean[] player4 = new boolean[9]; 
+            
             for (int j = 0; j < 9; j++) {
                 player1[j] = deck[j];
-                player2[j] = deck[j + 9];
-                player3[j] = deck[j + 18];
-                player4[j] = deck[j + 27];
             }
             if (hasTwoJokers(player1)) {
                 occ++;

0bde5e945b6cd84424cb381694b48e7d91be226e
S <s>
2024-01-29 17:30:49 +0400

    {"types":"C","runs":51}

--- zero2022.java
+++ zero2022.java
@@ -1,4 +1,31 @@
+import acm.program.ConsoleProgram;
 
-public class zero2022 {
+public class zero2022 extends ConsoleProgram {
+	public void run() {
+		int[][] m = { { 1, 2, 3 }, { 9, 7, 8 }, { 4, 5, 6 } };
+		zero(m);
+	}
 
+	private void zero(int[][] m) {
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[0].length; i++) {
+				if (m[i][j] == 0) {
+					int curri = i;
+					int currj = j;
+					for (int l = 0; l < m.length; l++) {
+						for (int x = 0; x < m[0].length; x++) {
+							m[curri][x] = 0;
+							m[l][currj] = 0;
+						}
+					}
+				}
+			}
+		}
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[0].length; j++) {
+				print(m[i][j] + " ");
+			}
+			println();
+		}
+	}
 }

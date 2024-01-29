b01a0f60a4dfacafadcb66eeefd54927598f5cf4
S <s>
2024-01-29 17:14:41 +0400

    {"types":"C","runs":48}

--- carTest.java
+++ carTest.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class carTest {
+public class carTest extends ConsoleProgram {
+	public void run() {
+		testCar();
+	}
 
+	private void testCar() {
+		car2021 myCar = new car2021(10, 0);
+		while (!myCar.isBrokenDown()) {
+			boolean drove = myCar.turnOnAndDrive(10);
+			if (!drove && !myCar.isBrokenDown()) {
+				myCar.fillGas(10);
+				println("Gas needed, kms: " + myCar.getMileage());
+			}
+		}
+
+		println(myCar.getMileage());
+	}
 }
--- kitchen2021.java
+++ kitchen2021.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+import java.util.Map;
 
-public class kitchen2021 {
+import acm.program.ConsoleProgram;
 
+public class kitchen2021 extends ConsoleProgram {
+	public void run() {
+
+	}
+
+	private ArrayList<String> kitchen(Map<String, ArrayList<String>> receipts, ArrayList<String> ingredients) {
+		ArrayList<String> result = new ArrayList<>();
+		for (String key : receipts.keySet()) {
+			if (canBeCooked(receipts.get(key), ingredients)) {
+				result.add(key);
+			}
+		}
+		return result;
+	}
+
+	private boolean canBeCooked(ArrayList<String> neededIngredients, ArrayList<String> ourIngredients) {
+		for(String ing: neededIngredients) {
+			if(!ourIngredients.contains(ing)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }

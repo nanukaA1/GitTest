6a636f002817a7da6e57c708ddd807d3b5202807
S <s>
2024-01-30 13:59:29 +0400

    {"types":"C","runs":83}

--- joker2014.java
+++ joker2014.java
@@ -6,31 +6,7 @@ public class joker2014 extends ConsoleProgram {
 	}
 
 	private double simulateJoker() {
-		
-	  double  occ = 0;
-		for (int i = 0; i < 10000; i++) {
-			 boolean[] deck = new boolean[36];
-		        deck[34] = true;
-		        deck[35] = true;
-		        
-		        for (int j = deck.length - 1; j > 0; j--) {
-		            int index = (int) (Math.random() * (i + 1));
-		            boolean temp = deck[index];
-		            deck[index] = deck[i];
-		            deck[i] = temp;
-		        }
-
-            boolean[] player1 = new boolean[9]; 
-            
-            for (int j = 0; j < 9; j++) {
-                player1[j] = deck[j];
-            }
-            if (hasTwoJokers(player1)) {
-                occ++;
-            }
-		
-	}
-		return occ;
+	  
 		
 }
 

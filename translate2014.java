04a9696f7e11819c4bbd78b78da6179d6d2b57cc
S <s>
2024-01-30 17:52:37 +0400

    {"types":"C","runs":114}

--- translate2014.java
+++ translate2014.java
@@ -8,10 +8,10 @@ public class translate2014 extends ConsoleProgram {
 	private String translateToTitle(String text) {
 		String translated = "";
 		String[] words = text.split(" ");
+		String[] words1 = new String[words.length];
 		for (int i = 0; i < words.length; i++) {
-			words[i] = words[i].toLowerCase();
-			Character.toUpperCase(words[i].charAt(0));
-			println(words[i]);
+			words1[i] =Character.toUpperCase(words[i].charAt(0))+ words[i].substring(1).toLowerCase();
+			println(words1[i]);
 		}
 		return translated;
 	}

e1dfc4e66519fbd516f853b11a3847211ad33c87
S <s>
2024-01-30 18:56:29 +0400

    {"types":"C","runs":122}

--- slideGallery2014.java
+++ slideGallery2014.java
@@ -6,7 +6,7 @@ import acm.util.ErrorException;
 import javafx.scene.input.MouseEvent;
 
 public class slideGallery2014 extends GraphicsProgram {
-	
+
 	public static final int IMG_H = 200;
 	public static final int IMG_DST = 20;
 	private boolean allPics = true;
@@ -16,56 +16,54 @@ public class slideGallery2014 extends GraphicsProgram {
 
 	public void run() {
 		addImages();
-		update();
-	}	
-	
-	public void mouseClicked(MouseEvent e){
-		if(allPics){
+		//update();
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		if (allPics) {
 			allPics = false;
-		}else{
-			if(e.getX() < getWidth()/3){
-				
-			}else if(e.getX()>getWidth()/3 && e.getX() < getWidth()*2/3){
+		} else {
+			if (e.getX() < getWidth() / 3) {
+
+			} else if (e.getX() > getWidth() / 3 && e.getX() < getWidth() * 2 / 3) {
 				allPics = true;
-			}else if(e.getX()> getWidth()*2/3){
-				
+			} else if (e.getX() > getWidth() * 2 / 3) {
+
 			}
 		}
 		update();
-			
+
 	}
+
 	private void update() {
 		removeAll();
-		if(allPics){
+		if (allPics) {
 			drawImages();
-		}else{
+		} else {
 			drawOne();
 		}
 	}
 
-
 	private void drawOne() {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	private void drawImages() {
 		int x = IMG_DST;
 		int y = IMG_DST;
-		for(int i = 0; i < images.size(); i ++){
+		for (int i = 0; i < images.size(); i++) {
 			GImage newImage = new GImage(images.get(i));
 			newImage.setSize(IMG_H, IMG_H);
-			newImage.setLocation(x,y);
+			newImage.setLocation(x, y);
 			add(newImage);
 			x += IMG_H + IMG_DST;
-			if(x + IMG_H + IMG_DST > getWidth()){
+			if (x + IMG_H + IMG_DST > getWidth()) {
 				y += IMG_H + IMG_DST;
 				x = IMG_DST;
 			}
 		}
 	}
-
 
 	private void addImages() {
 		images.add("MehranS.jpg");

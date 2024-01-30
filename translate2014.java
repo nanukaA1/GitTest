e706d8151921970194d2e5c5d0a35b8ebb4aee2a
S <s>
2024-01-30 17:47:37 +0400

    {"types":"C","runs":112}

--- translate2014.java
+++ translate2014.java
@@ -1,16 +1,16 @@
 import acm.program.ConsoleProgram;
 
-public class translate2014 extends ConsoleProgram{
+public class translate2014 extends ConsoleProgram {
 	public void run() {
 		translateToTitle("etErNAl sUNShiNe OF tHe Spotless mIND");
 	}
-	private String translateToTitle(String text){
+
+	private String translateToTitle(String text) {
 		String translated = "";
 		String[] words = text.split(" ");
-		for(int i = 0; i < words.length; i++) {
-			 words[i].toLowerCase();
-			// words[i].substring(0,1).toUpperCase();
-			 println(words[i]);
+		for (int i = 0; i < words.length; i++) {
+			words[i] = words[i].toLowerCase();
+			println(words[i]);
 		}
 		return translated;
 	}

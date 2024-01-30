ace7b7116ecc15f8d942ec8b4c1f521b0c743f08
S <s>
2024-01-30 18:38:01 +0400

    {"types":"C","runs":117}

--- countAB2018.java
+++ countAB2018.java
@@ -1,4 +1,43 @@
+import java.util.HashSet;
 
-public class countAB2018 {
+import acm.program.ConsoleProgram;
 
+public class countAB2018 extends ConsoleProgram {
+	private HashSet<String> uniqueStrings = new HashSet<>();
+	public void run() {
+		countAB("abab");
+	}
+
+	private int countAB(String string) {
+		for(int i = 0; i < string.length(); i++) {
+			for(int j = i+2; j <= string.length(); j++){
+				String substring = string.substring(i, j);
+				if(countA(substring) == countB(substring)){
+					uniqueStrings.add(substring);
+				}
+			}
+		}
+		return uniqueStrings.size();
+		
+	}
+
+	private int countA(String substring) {
+		int count = 0;
+		for(int i = 0; i< substring.length();i++) {
+			if(substring.charAt(i) =='a') {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	private int countB(String substring) {
+		int count = 0;
+		for(int i = 0; i< substring.length();i++) {
+			if(substring.charAt(i) =='b') {
+				count++;
+			}
+		}
+		return count;
+	}
 }

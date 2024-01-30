6c6d21da0ef09ee4ad281708b73ecfc4559214b7
S <s>
2024-01-30 14:24:55 +0400

    {"types":"C","runs":88}

--- yvelazeX.java
+++ yvelazeX.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class yvelazeX {
+public class yvelazeX extends ConsoleProgram{
+	public void run() {
+		int[][] m = {
+	            { 0, 1, 0, 0, 1},
+	            { 1, 1, 1, 0, 1},
+	            { 0, 1, 0, 0, 1},
+	            { 1, 1, 1, 1, 1},
+	            { 0, 1, 1, 0, 1},
+	            { 0, 1, 0, 0, 1},
+	            { 0, 1, 0, 0, 1}
+	        };
+		findX(m);
+	}
 
+	private void findX(int[][] m) {
+		int size = 1;
+		
+	}
 }

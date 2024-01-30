b174cf6b4a7a5721fc28e9a09d0550411ed4e667
S <s>
2024-01-30 14:10:37 +0400

    {"types":"C","runs":86}

--- yvelazeplis2018.java
+++ yvelazeplis2018.java
@@ -4,56 +4,62 @@ import java.util.Iterator;
 import acm.program.ConsoleProgram;
 
 public class yvelazeplis2018 extends ConsoleProgram {
-	HashSet<Integer> plusSizes = new HashSet<>();
+    HashSet<Integer> plusSizes = new HashSet<>();
 
-	public void run() {
-		int[][] m = { { 00000100 }, { 00001110 }, { 00100100 }, { 01111111 }, { 00100110 }, { 00100100 } };
+    public void run() {
+        int[][] m = {
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 1, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1},
+            {0, 0, 1, 0, 0, 1, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 0}
+        };
 
-		findPlus(m);
+        findPlus(m);
+    }
 
-	}
+    private void findPlus(int[][] m) {
+        int rows = m.length;
+        int cols = m[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (m[i][j] == 1) {
+                    int size = findPlus(m, i, j, rows, cols);
+                    plusSizes.add(size);
+                }
+            }
+        }
+        println(maxHashSet());
+    }
 
-	private void findPlus(int[][] m) {
-		int rows = m.length;
-		int cols = m[0].length;
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				if (m[i][j] == 1) {
-					int size = findPlus(m, i, j, rows, cols);
-					plusSizes.add(size);
-				}
-			}
-		}
-		println(maxHashSet());
-	}
+    private int maxHashSet() {
+        Iterator<Integer> iterator = plusSizes.iterator();
+        int max = 0;
 
-	private int maxHashSet() {
-		Iterator<Integer> iterator = plusSizes.iterator();
-		int max = 0;
+        while (iterator.hasNext()) {
+            int current = iterator.next();
+            if (current > max) {
+                max = current;
+            }
+        }
 
-		while (iterator.hasNext()) {
-			int current = iterator.next();
-			if (current > max) {
-				max = current;
-			}
-		}
+        return max;
+    }
 
-		return max;
-	}
+    private int findPlus(int[][] m, int i, int j, int rows, int cols) {
+        int size = 1;
+        int left = j - 1, right = j + 1, up = i - 1, down = i + 1;
 
-	private int findPlus(int[][] m, int i, int j, int rows, int cols) {
-		int size = 1;
-		int left = j - 1, right = j + 1, up = i - 1, down = i + 1;
+        while (left >= 0 && right < cols && up >= 0 && down < rows && m[i][left] == 1 && m[i][right] == 1
+                && m[up][j] == 1 && m[down][j] == 1) {
+            size++;
+            left--;
+            right++;
+            up--;
+            down++;
+        }
 
-		while (left >= 0 && right < cols && up >= 0 && down < rows && m[i][left] == 1 && m[i][right] == 1
-				&& m[up][j] == 1 && m[down][j] == 1) {
-			size++;
-			left--;
-			right++;
-			up--;
-			down++;
-		}
-
-		return size;
-	}
+        return size;
+    }
 }

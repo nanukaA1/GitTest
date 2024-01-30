1d6db095a538ef5e76701cf54260ced38eb69b75
S <s>
2024-01-30 14:32:26 +0400

    {"types":"C","runs":89}

--- yvelazeX.java
+++ yvelazeX.java
@@ -1,6 +1,10 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
 import acm.program.ConsoleProgram;
 
 public class yvelazeX extends ConsoleProgram{
+	HashSet<Integer> xSizes = new HashSet<>();
 	public void run() {
 		int[][] m = {
 	            { 0, 1, 0, 0, 1},
@@ -11,11 +15,46 @@ public class yvelazeX extends ConsoleProgram{
 	            { 0, 1, 0, 0, 1},
 	            { 0, 1, 0, 0, 1}
 	        };
-		findX(m);
+		findXs(m);
 	}
 
-	private void findX(int[][] m) {
-		int size = 1;
-		
+	private void findXs(int[][] m) {
+		for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                if (m[i][j] == 1) {
+                    int size = findX(m, i, j, m.length, m[0].length);
+                    xSizes.add(size);
+                }
+            }
+        }
+        println(maxHashSet());
+	}
+
+	private int maxHashSet() {
+		Iterator<Integer> iterator = xSizes.iterator();
+        int max = 0;
+
+        while (iterator.hasNext()) {
+            int current = iterator.next();
+            if (current > max) {
+                max = current;
+            }
+        }
+
+        return max;
+	}
+
+	private int findX(int[][] m, int i, int j, int rows, int cols) {
+		 int size = 1;
+	     int left = j - 1, right = j + 1, up = i - 1, down = i + 1;
+	     while(left >= 0 && right <= cols && up >=0 && down <= rows && m[down][left] == 1 && m[down][right] == 1 && m[up][left] ==1
+	    		 && m[up][right] == 1) {
+	    	 size++;
+	            left--;
+	            right++;
+	            up--;
+	            down++;
+	     }
+	     return size;
 	}
 }

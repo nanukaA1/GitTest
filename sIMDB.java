b0bc05ed8adfeb091613b64e4ed09c32586892b8
S <s>
2024-01-30 19:28:18 +0400

    {"types":"AR","runs":132}

---
+++ sIMDB.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import acm.graphics.GImage;
+import acm.program.ConsoleProgram;
+
+public class sIMDB extends ConsoleProgram{
+	private HashMap<String, HashSet<String>> movieActors;
+	private HashMap<String, HashSet<String>> actorMovies;
+	public IMDB(){
+		
+	}
+	
+	public void addMovie(String title){
+		
+	}
+	
+	public void addActor(String movieTitle, String actor){
+		
+	}
+	public Iterator<String> getMovies(String actor){
+		
+	}
+	
+
+	public Iterator<String> getCommonMovies(String actor1, String actor2){
+		
+	}
+	
+	public Iterator<String> getCast(String movie, int n){
+		
+	}
+	
+}

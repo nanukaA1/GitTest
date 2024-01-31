81c430543c10cce7c5b2b9e9a1e30c361f641503
S <s>
2024-02-01 03:56:37 +0400

    {"types":"C","runs":29}

--- raffle.java
+++ raffle.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class raffle {
+public class raffle extends ConsoleProgram {
+	public void run() {
+		checkRaffleDistance("sds","sds");
+	}
 
+	private void checkRaffleDistance(String winningVal, String yourVal) {
+		int count = 0;
+		for(int i = 0; i < winningVal.length(); i++) {
+			if(winningVal.charAt(i) != yourVal.charAt(i)) {
+				count++;
+			}
+		}
+		
+		if(count == 0) {
+			println("you win");
+		} else if(count == 1 || count == 2) {
+			println("small prize");
+		} else{
+			println("nothing");
+		}
+	}
 }

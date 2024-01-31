0beb3506c6724bd43ad19b60d9f60dd547708ea0
S <s>
2024-02-01 03:56:36 +0400

    {"types":"C","runs":29}

--- kMer.java
+++ kMer.java
@@ -1,4 +1,19 @@
+import java.util.HashSet;
 
-public class kMer {
+import acm.program.ConsoleProgram;
 
+public class kMer extends ConsoleProgram {
+	private HashSet<String> frequent = new HashSet<>();
+
+	public void run() {
+		println(mostfrequentKmer("BOBOK", 2));
+	}
+
+	private Object mostfrequentKmer(String string, int k) {
+		for (int i = 0; i < string.length(); i++) {
+			for (int j = i + k; j <= string.length(); j++) {
+				
+			}
+		}
+	}
 }

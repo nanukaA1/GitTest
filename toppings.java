e8ea3afc45b64459d08deb6cae535719399f7eec
S <s>
2024-02-01 09:41:15 +0400

    {"types":"C","runs":33}

--- toppings.java
+++ toppings.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class toppings {
+import acm.program.ConsoleProgram;
 
+public class toppings extends ConsoleProgram{
+	HashMap<String, ArrayList<String>> toppings = new HashMap<>();
+	public void run() {
+		addToppings();
+	}
+	private void addToppings() {
+		while(true) {
+			String customerName = readLine("enter name: ");
+			if(customerName.equals("")){
+				break;
+			}
+			toppings.put(customerName, new ArrayList<>());
+			while(true) {
+				String topping = readLine("enter topping: ");
+				if(topping.equals("")){
+					break;
+				}
+				toppings.get(customerName).add(topping);
+			}
+		}
+	}
 }
